a3ad70732f032407477dadc8936e62b0db089a04
James Monks <J.monks23@gmail.com>
2020-07-28 18:38:35 +0100

    initial commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue({
+    el: '#app',
+    data: {},
+
+    computed: {},
+
+    mounted() {},
+
+    methods: {}
+    
+  })
+})
